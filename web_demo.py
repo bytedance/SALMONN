07e982a14380e7c07f7cc996f6d8dde1f572c7c7
tcl <tcl20@mails.tsinghua.edu.cn>
2024-04-08 11:23:49 +0800

    chore: add license

--- web_demo.py
+++ web_demo.py
@@ -1,4 +1,4 @@
-# Copyright (2023) Tsinghua University, Bytedance Ltd. and/or its affiliates
+# Copyright (2024) Tsinghua University, Bytedance Ltd. and/or its affiliates
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
